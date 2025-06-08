48d58e22f18b4136595b53ad8f42434fede48d08
Leo Tang <leotang634@gmail.com>
2025-06-07 23:24:56 -0400

    Added .gitignore

---
+++ UTS Open/UTSO 24/P2HappyGifts.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> hi(n);
+    for (int i = 0; i < n; i++) cin >> hi[i];
+    sort(hi.begin(), hi.end(), )
+}
