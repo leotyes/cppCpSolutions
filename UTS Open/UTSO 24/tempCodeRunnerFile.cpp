58e894e83581b32780fd94b7e349413cf5314a7a
Leo Tang <leotang634@gmail.com>
2025-06-08 12:06:57 -0400

    Solved Happy Gifts

--- UTS Open/UTSO 24/tempCodeRunnerFile.cpp
+++ UTS Open/UTSO 24/tempCodeRunnerFile.cpp
@@ -9,7 +9,7 @@ int main() {
     cin.sync_with_stdio(0);
     cin.tie(0);
     int n, k, in; 
-    long long m = 0;
+    long long m = -1;
     cin >> n >> k;
     vector<int> pns, nns;
     for (int i = 0; i < n; i++) {
@@ -23,13 +23,13 @@ int main() {
     sort(pns.begin(), pns.end(), greater<int>());
     sort(nns.begin(), nns.end());
     vector<long long> ppsa(pns.size() + 1, 0), npsa(nns.size() + 1, 0);
-    for (int i = 1; i < (int)ppsa.size(); ++i) {
+    for (int i = 1; i < ppsa.size(); ++i) {
         ppsa[i] = ppsa[i - 1] + pns[i - 1];
     }
-    for (int i = 1; i < (int)npsa.size(); ++i) {
+    for (int i = 1; i < npsa.size(); ++i) {
         npsa[i] = npsa[i - 1] + nns[i - 1];
     }
-    for (int i = 0; i <= min(k, (int)pns.size()); ++i) {
+    for (int i = 0; i < min(k, (int)pns.size()); ++i) {
         m = max(m, ppsa[i] - npsa[min((k - i) / 2, (int)nns.size())]);
     }
     cout << m << "\n";
