6940a3a9fbc91eac1fd61c5fe28e5dc5db23f944
Leo Tang <leotang634@gmail.com>
2025-10-11 21:51:31 -0400

    Partials on S3 Swipe, this is too hard

--- CCC/CCC 24/S3Swipe.cpp
+++ CCC/CCC 24/S3Swipe.cpp
@@ -6,13 +6,75 @@ int main() {
     cin.sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
+    int n, ans = 0, cc = 0;
 
     cin >> n;
 
-    vector<int> a(n), b(n);
+    vector<int> a(n), b(n), breaks;
+    queue<int> brm, brmc;
+    queue<tuple<char, int, int>> lm;
+    stack<tuple<char, int, int>> rm;
 
     for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
 
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+        if (brm.empty()) {
+            brm.push(b[i]);
+        } else if (brm.back() != b[i]) {
+            breaks.push_back(i - 1);
+            brm.push(b[i]);
+        }
+    }
+
+    brmc = brm;
+
+    for (int i = 0; i < n; i++) {
+        if (brm.front() == a[i]) {
+            brm.pop();
+        }
+    }
+
+    if (brm.empty()) {
+        cout << "YES" << "\n";
+    } else {
+        cout << "NO";
+        return 0;
+    }
+
+    while (!brmc.empty()) {
+        int cura = find(a.begin(), a.end(), brmc.front()) - a.begin();
+        int curb = find(b.begin(), b.end(), brmc.front()) - b.begin();
+        if (cura > curb) {
+            lm.emplace('L', curb, cura);
+            ans++;
+        }
+        if (cc < breaks.size()) {
+            if (cura < breaks[cc]) {
+                rm.emplace('R', cura, breaks[cc]);
+                ans++;
+            }
+        } else {
+            if (cura < n - 1) {
+                rm.emplace('R', cura, n - 1);
+                ans++;
+            }
+        }
+        cc++;
+        brmc.pop();
+    }
+
+    cout << ans << "\n";
+
+    while (!lm.empty()) {
+        cout << get<0>(lm.front()) << " " << get<1>(lm.front()) << " " << get<2>(lm.front()) << "\n";
+        lm.pop();
+    }
+
+    while (!rm.empty()) {
+        cout << get<0>(rm.top()) << " " << get<1>(rm.top()) << " " << get<2>(rm.top()) << "\n";
+        rm.pop();
     }
 }
