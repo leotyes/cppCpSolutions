19972833fdebd9e326b078d7b19de9a8623af5fd
Leo Tang <leotang634@gmail.com>
2025-10-26 21:08:55 -0400

    Solved J3 Secret Instructions

--- CCC/CCC 21/J3SecretInstructions.cpp
+++ CCC/CCC 21/J3SecretInstructions.cpp
@@ -1,3 +1,28 @@
-//
-// Created by roblo on 2025-10-26.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    string prev;
+
+    while (true) {
+        string s;
+
+        cin >> s;
+
+        if (s == "99999") break;
+
+        if (s[0] == '0' && s[1] == '0') {
+            cout << prev << s.substr(2, 3);
+        } else if (((int)s[0] + (int)s[1]) % 2 == 0) {
+            cout << "right " << s.substr(2, 3);
+            prev = "right ";
+        } else {
+            cout << "left " << s.substr(2, 3);
+            prev = "left ";
+        }
+        cout << "\n";
+    }
+}
