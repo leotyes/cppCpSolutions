66138c49cb774f9654057aea2194fb7cdb808f9f
Leo Tang <leotang634@gmail.com>
2026-07-23 20:15:31 -0400

    Finished CCC 2014 S4

--- CCC/CCC 14/S4TintedGlassWindow.cpp
+++ CCC/CCC 14/S4TintedGlassWindow.cpp
@@ -7,39 +7,54 @@ signed main() {
     cin.tie(0);
 
     int n, t;
+    unsigned long long ans = 0;
     cin >> n >> t;
     map<pair<int, int>, int> diff;
-    unordered_map<int, int> mx, my, mxr, myr;
-    vector<vector<int>> vn(1001, vector<int>(1001, 0)), vdiff(1001, vector<int>(1001, 0));
+    set<int> xs, ys;
+    vector<vector<int>> vn(2005, vector<int>(2005, 0));
+    vector<vector<int>> vdiff(2005, vector<int>(2005, 0));
 
     for (int i = 0; i < n; i++) {
         int xl, yt, xr, yb, ti;
         cin >> xl >> yt >> xr >> yb >> ti;
+
         diff[make_pair(yt, xl)] += ti;
-        diff[make_pair(yt, xr + 1)] -= ti;
-        diff[make_pair(yb + 1, xl)] += ti;
-        diff[make_pair(yb + 1, xr + 1)] -= ti;
+        diff[make_pair(yt, xr)] -= ti;
+        diff[make_pair(yb, xl)] -= ti;
+        diff[make_pair(yb, xr)] += ti;
+
+        xs.insert(xl);
+        xs.insert(xr);
+        ys.insert(yt);
+        ys.insert(yb);
     }
 
-    int prevx = 0, prevy = 0;
-    for (auto [p, t] : diff) {
-        auto [y, x] = p;
-        if (myr.find(y) == myr.end()) {
-            prevy++;
-            myr[y] = prevy;
-            my[prevy] = y;
-        }
-        if (mxr.find(x) == mxr.end()) {
-            prevx++;
-            mxr[x] = prevx;
-            mx[prevx] = x;
-        }
-    }
+    unordered_map<int, int> mxr, myr;
+
+    int id = 0;
+    for (auto x : xs) mxr[x] = id++;
+    id = 0;
+    for (auto y : ys) myr[y] = id++;
 
     for (auto [p, t] : diff) {
         auto [y, x] = p;
-        vdiff[y][x] = t;
+        vdiff[myr[y]][mxr[x]] = t;
     }
 
+    for (int i = 0; i < ys.size(); i++) {
+        for (int j = 0; j < xs.size(); j++) {
+            vn[i + 1][j + 1] = vn[i][j + 1] + vn[i + 1][j] - vn[i][j] + vdiff[i][j];
+        }
+    }
 
+    vector<int> vx(xs.begin(), xs.end()), vy(ys.begin(), ys.end());
+    for (int i = 0; i + 1 < ys.size(); i++) {
+        for (int j = 0; j + 1 < xs.size(); j++) {
+            if (vn[i + 1][j + 1] >= t) {
+                ans += (__int128)(vy[i + 1] - vy[i]) * (vx[j + 1] - vx[j]);
+            }
+        }
+    }
+
+    cout << ans;
 }
